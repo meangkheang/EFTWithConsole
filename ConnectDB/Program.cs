af96ff209311231f6de733d651cca568ea878b3b
im 2cccccc <im2cccccc@outlook.com>
2023-02-28 22:27:31 +0700

    add context file

--- ConnectDB/Program.cs
+++ ConnectDB/Program.cs
@@ -1,13 +1,5 @@
 ﻿using ConnectDB.AppContexts;
 using ConnectDB.Models;
 
-using (var context = new BloggingContext())
-{
-    // Create the database if it doesn't already exist
-    context.Database.EnsureCreated();
-
-    // Add a new Blog object
-    var blog = new Blog { Url = "http://example.com" };
-    context.Blogs.Add(blog);
-    context.SaveChanges();
-}
+// new to add every context in this program file when add migration.
+//BloggingContext ctx =new BloggingContext();
