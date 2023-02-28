c39b86d936f15473be83a583c4bae84624caf96b
im 2cccccc <im2cccccc@outlook.com>
2023-02-28 22:00:26 +0700

    Add project files.

---
+++ ConnectDB/AppContexts/BloggingContext.cs
@@ -0,0 +1,21 @@
+﻿using ConnectDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConnectDB.AppContexts
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ConsoleEFCore;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ ConnectDB/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConnectDB.AppContexts;
+using ConnectDB.Models;
+
+using (var context = new BloggingContext())
+{
+    // Create the database if it doesn't already exist
+    context.Database.EnsureCreated();
+
+    // Add a new Blog object
+    var blog = new Blog { Url = "http://example.com" };
+    context.Blogs.Add(blog);
+    context.SaveChanges();
+}
